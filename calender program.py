8068412f6ff8cf3f130929a5cc1ba5fc28eed384
bonny98 <bonnydavid98@gmail.com>
2017-05-18 16:01:47 +0300

    Add files via upload
    asks the user to input his birthdate and generates the weekday he or she was born

---
+++ calender program.py
@@ -0,0 +1,21 @@
+import datetime as dt
+
+#ask the user to input details of birth
+year=input("please enter year of birth(format yyyy): \n")
+month=input("please enter month of birth(format mm): \n")
+date=input("please enter day of birth(format dd): \n")
+
+birthday=dt.date(int(year), int(month), int(date))
+print(birthday)
+
+week_day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+  'Friday', 'Saturday', 'Sunday']
+
+sf = "If you were born on %s, you were born on a %s"
+print(sf % (birthday, week_day[dt.date.weekday(birthday)]))
+print("thank you for using this program press enter to exit")
+input()
+
+
+
+
